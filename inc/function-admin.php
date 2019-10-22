7fb331d904d3933887564e5073070f41fb565dee
sohan <shansohansingh@gmail.com>
2019-10-22 10:04:18 +0530

    Add custom options with setting API

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -16,8 +16,27 @@ function sunset_add_admin_page() {
 
    add_submenu_page( 'alecaddd_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'alecaddd_sunset_css', 'sunset_theme_setting_page' );
 
+   //Activate custom setting
+   add_action( 'admin_init', 'sunset_custom_settings' );
 }
 add_action( 'admin_menu', 'sunset_add_admin_page');
+
+function sunset_custom_settings() {
+    register_setting( 'sunset-setting-group', 'first_name' );
+
+    add_settings_section( 'sunset-sidebar-options', 'Sidebar Option', 'sunset_sidebar_options', 'alecaddd_sunset' );
+
+    add_settings_field( 'sidebar-name', 'First Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options' );
+}
+
+function sunset_sidebar_options() {
+    echo "customize properties";
+}
+
+function sunset_sidebar_name() {
+    $firstName = esc_attr( get_option( 'first_name' ) );
+    echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" />';
+}
 
 function sunset_theme_create_page() {
    require_once( get_template_directory() . '/inc/templates/sunset-admin.php' );
@@ -27,4 +46,5 @@ function sunset_theme_setting_page() {
     // generation  of our admin page
     echo "<h1>Welcome custom css</h1>";
 }
+
 ?>
--- inc/templates/sunset-admin.php
+++ inc/templates/sunset-admin.php
@@ -1,2 +1,7 @@
-<h1>Sunset Admin</h1>
-<?php bloginfo('name'); ?>
+<h1>Sunset Theme Setting</h1>
+<?php settings_errors(); ?>
+<form action="options.php" method="post">
+    <?php settings_fields( 'sunset-setting-group' ); ?>
+    <?php do_settings_sections( 'alecaddd_sunset' ); ?>
+    <?php submit_button(); ?>
+</form>
